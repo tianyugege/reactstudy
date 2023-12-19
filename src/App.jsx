3e4bd2ef597771dd8fbe6e202889bf2aec55b792
guoguo <guotianyu321@gmail.com>
2023-12-19 19:50:11 +0800

    feat:init project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const App=()=>{
+  const now = new Date()
+  const a = 10
+  const b = 20
+  console.log(now,a+b)
+  return(
+    <div class='my-app'>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a+b}
+      </p>
+    </div>
+  )
+}
+
+export default App
